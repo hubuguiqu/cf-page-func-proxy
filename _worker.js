6ef72500a7852c6a37c29893c70117bfbf9ad6a4
hubuguiqu <80161848+hubuguiqu@users.noreply.github.com>
2025-07-28 10:55:45 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="tom.renzhe.xyz";
+        url.hostname="http://tom.renzhe.xyz";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
